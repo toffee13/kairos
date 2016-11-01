914893e4dee2f63e89df41796244c0ae97999de5
daehwan <daehwann@gmail.com>
2016-11-01 14:40:18 +0900

    Course model http added

--- models/course.js
+++ models/course.js
@@ -1,13 +1,55 @@
-var http = require('http');
+var https = require('https');
 
 exports.list = function(page){
 
+    console.log(process.env);
 
     return [];
 }
 
-exports.get = function(id) {
+exports.get = function(id, callback) {
+    //https://api.vimeo.com/me/videos/{video_id}
+    var id = 188947289;
+
+    var options = {
+        hostname: 'api.vimeo.com',
+        path: `/me/videos`,
+        headers: {
+            'Authorization': `Bearer ${process.env.VIMEO_TOKEN}`,
+        }
+    };
+
+    console.log(`Path: ${JSON.stringify(options)}`);
+
+    var req = https.get(options, (res) => {
+
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`DATA: ${res.data}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+
+        var body = '';
+        res.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        res.on('end', () => {
+            var jsonBody = JSON.parse(body);
+            console.log(`BODY: ${JSON.stringify(jsonBody)}`);
+
+            console.log('No more data in response.');
+
+            callback(jsonBody);
+        });
+    });
+
+    req.on('error', (e) => {
+
+        console.log(`problem with request: ${e.message}`);
+
+        callback({});
+    });
+
+    req.end();
 
 
-    return {};
 }
--- routes/course.js
+++ routes/course.js
@@ -5,9 +5,10 @@ var Course = require('../models/course');
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
 
-  var course = Course.get(req.params.id);
+  var course = Course.get(req.params.id, (course) => {
+    res.render('course', { title: 'Express', course: JSON.stringify(course) });
+  });
 
-  res.render('course', { title: 'Express', course: course });
 });
 
 module.exports = router;
