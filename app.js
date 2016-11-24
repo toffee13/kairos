fa0c83951d90cbeae2f7c02048f7658029dc5fe8
SunKyungCho <sunkyungch@gmail.com>
2016-11-24 22:04:47 +0900

    master app.js 머지

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session')
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var course = require('./routes/course');
